1a7164a94f08ef9d2eba1c0d0b3e71c282658f99
Siddeeq <Siddeeq@DESKTOP-RIF12TV.fios-router.home>
2017-10-09 19:32:00 -0400

    finished what i wanna do

--- ChatbotProject/src/chatbotProject/SiddeeqChatbot.java
+++ ChatbotProject/src/chatbotProject/SiddeeqChatbot.java
@@ -2,8 +2,11 @@ package chatbotProject;
 
 public class SiddeeqChatbot implements Topic {
 
-	private String[] keywords = {"type","battery","batteries","resolution","resolutions","iphone","iphones","samsung","samsungs","lg","htc","motorola","motorolas","moto"};
+	private String[] keywords = {"type","battery","batteries","camera","cameras","iphone","iphones","samsung","samsungs","lg","htc","motorola","motorolas","moto","android","androids"};
 	private String[] goodbyeWords = {"bye", "goodbye", "stop"};
+	private String[] yesWords = {"yes", "indeed", "okay", "please do", "go ahead","please", "why not", "sure", "alright"};
+	private String[] cameraFacts = {"Major manufacturers of cameras for phones include Toshiba, ST Micro, Sharp, Omnivision, and Aptina","Phones like  Apple’s iPhone 7 Plus and the new OnePlus 5 feature two rear facing cameras to enhance their zooming capabilities.","The original iPhone and the HTC Dream (T-Mobile G1) were the first smartphones, in the modern sense of the word, and both arrived with cameras onboard."};
+	private int camFactPsn = cameraFacts.length - 1;
 	private String previousResponse = "";
 	private String chatTopic = "";
 	private boolean chatting;
@@ -17,10 +20,23 @@ public class SiddeeqChatbot implements Topic {
 	}
 
 	@Override
-	public boolean isTriggered(String response) {
+	public boolean isTriggered(String response) 
+	{
 		for (int i = 0; i<keywords.length; i++)
 		{
-			if (ChatbotMain.findKeyword(response, keywords[i], 0) >= 0)
+			if (ChatbotMain.findKeyword(response.toLowerCase(), keywords[i], 0) >= 0)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public boolean isYes(String s)
+	{
+		for (int i = 0; i<yesWords.length; i++)
+		{
+			if (ChatbotMain.findKeyword(s.toLowerCase(), yesWords[i], 0) >= 0)
 			{
 				return true;
 			}
@@ -48,10 +64,40 @@ public class SiddeeqChatbot implements Topic {
 		temp = temp+"\"";
 		return temp;
 	}
+	public String fetchCameraFact()
+	{
+		if (camFactPsn<0)
+		{
+			return null;
+		}
+		else
+		{
+			
+			if (camFactPsn==2)
+			{
+				camFactPsn--;
+				return cameraFacts[2];
+			}
+			else
+			{
+				if (camFactPsn==1)
+				{
+					camFactPsn--;
+					return cameraFacts[1];
+				}
+				else
+				{
+						camFactPsn--;
+						return cameraFacts[0];	
+				}
+			}
+		}
+	}
 
 	@Override
-	public void startChatting(String response) {
-		ChatbotMain.print("Looks like you want to discuss types of devices! What would you like to talk about?");
+	public void startChatting(String response) 
+	{
+		ChatbotMain.print("Looks like you want to discuss types of devices! What would you like to talk about? (keywords include battery life, cameras, and phone brands.)");
 		chatting = true;
 		while (chatting) 
 		{
@@ -98,6 +144,7 @@ public class SiddeeqChatbot implements Topic {
 						boolean relevant = false;
 						for (int i=0; i < keywords.length; i++)
 						{
+							response = response.toLowerCase();
 							if (ChatbotMain.findKeyword(response, keywords[i], 0) >= 0)
 							{
 								relevant = true;
@@ -111,22 +158,45 @@ public class SiddeeqChatbot implements Topic {
 								chatTopic = "battery";
 								ChatbotMain.print("I can rate the kind of battery you have for screen-on time! Do you know what milliamp hours (mAh) are?");
 							}
-							if (ChatbotMain.findKeyword(response, "resolution", 0) >= 0 || ChatbotMain.findKeyword(response, "resolutions", 0) >= 0)
+							else
 							{
-								chatTopic = "resolution";
-								//TODO
-								ChatbotMain.print("this is temporary  text about resolution cause idk what to do with it");
-								chatTopic = "";
-							}
-							if (ChatbotMain.findKeyword(response, "iphone", 0) >= 0 || ChatbotMain.findKeyword(response, "iphones", 0) >= 0)
-							{
-								chatTopic = "iphone";
-								ChatbotMain.print("Interested in iPhones, are we? Whats your favorite thing about them?");
+								if (ChatbotMain.findKeyword(response, "camera", 0) >= 0 || ChatbotMain.findKeyword(response, "cameras", 0) >= 0)
+								{
+									chatTopic = "camera";
+									ChatbotMain.print("Would you like to hear some facts about smartphone cameras?");
+								}
+								else
+								{
+									if (ChatbotMain.findKeyword(response, "iphone", 0) >= 0 || ChatbotMain.findKeyword(response, "iphones", 0) >= 0)
+									{
+										chatTopic = "iphone";
+										ChatbotMain.print("Interested in iPhones, are we? Whats your favorite thing about them?");
+									}
+									else
+									{
+										if (ChatbotMain.findKeyword(response, "samsung", 0) >= 0 || ChatbotMain.findKeyword(response, "samsungs", 0) >= 0 || ChatbotMain.findKeyword(response, "android", 0) >= 0 || ChatbotMain.findKeyword(response, "androids", 0) >= 0 || ChatbotMain.findKeyword(response, "lg", 0) >= 0 || ChatbotMain.findKeyword(response, "htc", 0) >= 0 || ChatbotMain.findKeyword(response, "moto", 0) >= 0 || ChatbotMain.findKeyword(response, "motorola", 0) >= 0 || ChatbotMain.findKeyword(response, "motorolas", 0) >= 0)
+										{
+											chatTopic = "android";
+											ChatbotMain.print("Interested in Androids, are we? Whats your favorite thing about them?");
+										}
+									}
+								}
 							}
 						}
 						else
 						{
-							ChatbotMain.print("Huh. I don't really get you. Tell me something else?");
+							if (repetition==0)
+							{
+								ChatbotMain.print("I'm afraid I'm unfamiliar with that, tell me something else please.");
+							}
+							if (repetition==1 || repetition==2)
+							{
+								ChatbotMain.print("Excuse me?");
+							}
+							if (repetition>2)
+							{
+								ChatbotMain.print("Oh my LORD please start making sense...");
+							}
 						}
 					}
 					else
@@ -167,34 +237,6 @@ public class SiddeeqChatbot implements Topic {
 								ChatbotMain.print("Please input a number.");
 							}
 						}
-						if (chatTopic=="iphone")
-						{
-							if (ChatbotMain.findKeyword(response, "like", 0) >=0)
-							{
-								iphoneLike = response.substring(ChatbotMain.findKeyword(response, "like", 0), ChatbotMain.findKeyword(response, "like", 0)+5);
-							}
-							else
-							{
-								iphoneLike = response;
-								
-							}
-							ChatbotMain.print("You know what, I like "+iphoneLike+" too! What do you hate about androids?");
-							chatTopic = "androidHate";
-						}
-						if (chatTopic=="android")
-						{
-							if (ChatbotMain.findKeyword(response, "like", 0) >=0)
-							{
-								androidLike = response.substring(ChatbotMain.findKeyword(response, "like", 0), ChatbotMain.findKeyword(response, "like", 0)+5);
-							}
-							else
-							{
-								androidLike = response;
-								
-							}
-							ChatbotMain.print("You know what, I like "+iphoneLike+" too! What do you hate about iphones?");
-							chatTopic = "iphoneHate";
-						}
 						if (chatTopic=="androidHate")
 						{
 							if (ChatbotMain.findKeyword(response, "hate", 0) >=0)
@@ -206,11 +248,12 @@ public class SiddeeqChatbot implements Topic {
 								androidHate = response;
 								
 							}
-							ChatbotMain.print("I know, I hate "+androidHate+" too! If only they were like certain OTHER devices. Everyone likes a phone that "+iphoneLike+", right? Lets talk about something else.");
+							ChatbotMain.print("I know, I agree! "+androidHate+"! If only they were like certain OTHER devices. Everyone likes a phone when "+iphoneLike+", right? Lets talk about something else.");
 							iphoneLike = "";
 							iphoneHate = "";
 							androidHate = "";
 							androidLike = "";
+							chatTopic = "";
 						}
 						if (chatTopic=="iphoneHate")
 						{
@@ -223,11 +266,60 @@ public class SiddeeqChatbot implements Topic {
 								iphoneHate = response;
 								
 							}
-							ChatbotMain.print("I know, I hate "+iphoneHate+" too! If only they were like certain OTHER devices. Everyone likes a phone that "+androidLike+", right? Lets talk about something else.");
+							ChatbotMain.print("I know, I hate  how "+iphoneHate+" too! If only they were like certain OTHER devices. Everyone likes a phone when "+androidLike+", right? Lets talk about something else.");
 							iphoneLike = "";
 							iphoneHate = "";
 							androidHate = "";
 							androidLike = "";
+							chatTopic = "";
+						}
+						if (chatTopic=="iphone")
+						{
+							if (ChatbotMain.findKeyword(response, "like", 0) >=0)
+							{
+								iphoneLike = response.substring(ChatbotMain.findKeyword(response, "like", 0)+5);
+							}
+							else
+							{
+								iphoneLike = response;
+								
+							}
+							ChatbotMain.print("Yeah, "+iphoneLike+" ! What do you hate about androids?");
+							chatTopic = "androidHate";
+						}
+						if (chatTopic=="android")
+						{
+							if (ChatbotMain.findKeyword(response, "like", 0) >=0)
+							{
+								androidLike = response.substring(ChatbotMain.findKeyword(response, "like", 0)+5);
+							}
+							else
+							{
+								androidLike = response;
+								
+							}
+							ChatbotMain.print("Yeah "+iphoneLike+" ! What do you hate about iphones?");
+							chatTopic = "iphoneHate";
+						}
+						if (chatTopic=="camera")
+						{
+							if (isYes(response))
+							{
+								if (camFactPsn>0)
+								{
+									ChatbotMain.print(fetchCameraFact()+" Would you like to hear another?");
+								}
+								else
+								{
+									ChatbotMain.print("I've told you all that I know about phone cameras. Lets talk about someting else.");
+									chatTopic = "";
+								}
+							}
+							else
+							{
+								ChatbotMain.print("Alright, what else do you wanna discuss?");
+								chatTopic = "";
+							}
 						}
 					}
 				}
